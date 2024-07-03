b798c4b72ff2ace760348b8edc62290257d1c17f
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-03 22:21:50 +0200

    Feat: URL connection

--- coreService/settings.py
+++ coreService/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'userservice',
   
 ]
 
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'coreService.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 
