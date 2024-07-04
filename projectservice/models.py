72d165ac2c43db23e0d048eec91f0ff4d48d6305
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-04 12:06:02 +0200

    Feat: Connection Functionality

--- projectservice/models.py
+++ projectservice/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ProjectService(models.Model):
+    endpoint = models.CharField(max_length=100, null=True, default=None)
+    last_communication = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.endpoint
