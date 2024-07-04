3c56d3c7fd7d3ea97fca63f5d5fa72ae49d74a62
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-04 13:12:39 +0200

    Feat: URL routing

--- projectservice/urls.py
+++ projectservice/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectServiceViewSet
+from .views import ProjectServiceViewSet, ProjectTeamServiceViewSet
 
-# Create a router and register the viewset
 router = DefaultRouter()
-router.register(r'projects', ProjectServiceViewSet)
+router.register(r'projects', ProjectServiceViewSet, basename='projectservice')
+router.register(r'teams', ProjectTeamServiceViewSet, basename='projectteamservice')
 
-# Include the router URLs in the urlpatterns
 urlpatterns = [
     path('', include(router.urls)),
 ]
